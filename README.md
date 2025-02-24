c98cec3454e31bfbf0060a2aed094df2a07ca865
eunli <eunli99012@gmail.com>
2025-02-25 06:22:59 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Bookmate
+
+독서 커뮤니티 플랫폼입니다.
+
+---
+
+## 🗒️ ERD
+
+![bookmate_erd.png](bookmate_erd.png)
+
+---
---
+++ src/test/java/com/bookmate/bookmate/BookmateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookmate.bookmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
